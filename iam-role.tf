b6ee738cdaf9b33a92f778cdb05eca3c9452f9af
xneyder <xneyder@gmail.com>
2023-11-09 20:21:30 -0500

    Fix

--- iam-role.tf
+++ iam-role.tf
@@ -6,6 +6,7 @@ resource "aws_iam_role" "this" {
   assume_role_policy   = var.assume_role_policy
   permissions_boundary = var.permissions_boundary
   path                 = var.iam_role_path
+  tags                 = var.iam_role_tags
 }
 
 # data "aws_iam_policy_document" "assume_role_policy" {
--- variables.tf
+++ variables.tf
@@ -276,3 +276,11 @@ variable "iam_role_path" {
   description = "Path in which to create the IAM role for the Lambda Function."
   default     = "/"
 }
+
+variable "iam_role_tags" {
+  type        = map(string)
+  description = <<EOF
+  A mapping of tags to assign to the resource. 
+  EOF
+  default     = {}
+}
